8939e874c71378bfb0d89bbf8f2bc97c1034a838
Nobuyuki Akama <nakama@microsoft.com>
2022-09-02 21:23:01 +0900

    プロジェクト ファイルを追加します。

---
+++ AzRefArc.AspNetBlazorServer.BlazorServer/Pages/Shared/AuthorOverview.cs
@@ -0,0 +1,11 @@
+﻿namespace AzRefArc.AspNetBlazorServer.BlazorServer.Pages.Shared
+{
+    public class AuthorOverview
+    {
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+        public bool Contract { get; set; }
+    }
+}
---
+++ AzRefArc.AspNetBlazorServer.BlazorServer/Pages/Shared/PublisherOverview.cs
@@ -0,0 +1,9 @@
+﻿namespace AzRefArc.AspNetBlazorServer.BlazorServer.Pages.Shared
+{
+    public class PublisherOverview
+    {
+        public string PublisherId { get; set; }
+        public string PublisherName { get; set; }
+        public List<TitleOverview> TitleOverviews { get; set; }
+    }
+}
