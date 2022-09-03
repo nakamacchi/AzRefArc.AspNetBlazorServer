218af7a500c42832adb127af21407405a3adf63d
Nobuyuki Akama <nakama@microsoft.com>
2022-09-03 15:12:02 +0900

    コミットミスを修正

---
+++ AzRefArc.AspNetBlazorServer.BlazorServer/Pages/Shared/PublisherOverview.cs
@@ -0,0 +1,9 @@
+﻿namespace AzRefArc.AspNetBlazorServer.BlazorServer.Pages.Shared
+{
+    public class PublisherOverview
+    {
+        public string PublisherId { get; set; } = String.Empty;
+        public string PublisherName { get; set; } = String.Empty;
+        public List<TitleOverview> TitleOverviews { get; set; } = new List<TitleOverview>();
+    }
+}
