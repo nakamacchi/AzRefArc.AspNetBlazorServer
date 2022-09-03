de0d736b55940cb945f701cfd3f388d24faeca8e
Nobuyuki Akama <nakama@microsoft.com>
2022-09-03 15:08:40 +0900

    最新版にリプレース

--- AzRefArc.AspNetBlazorServer.BlazorServer/Pages/Shared/PublisherOverview.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzRefArc.AspNetBlazorServer.BlazorServer.Pages.Shared
-{
-    public class PublisherOverview
-    {
-        public string PublisherId { get; set; }
-        public string PublisherName { get; set; }
-        public List<TitleOverview> TitleOverviews { get; set; }
-    }
-}
