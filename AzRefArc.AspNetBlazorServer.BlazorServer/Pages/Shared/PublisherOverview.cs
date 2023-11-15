6c760d1ec53d0587913bc6e48b187d16ffd9d359
Nobuyuki Akama <nakama@microsoft.com>
2023-11-15 09:20:54 +0900

    .NET8 ソースへの入れ替えのためファイルを削除

--- AzRefArc.AspNetBlazorServer.BlazorServer/Pages/Shared/PublisherOverview.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzRefArc.AspNetBlazorServer.BlazorServer.Pages.Shared
-{
-    public class PublisherOverview
-    {
-        public string PublisherId { get; set; } = String.Empty;
-        public string PublisherName { get; set; } = String.Empty;
-        public List<TitleOverview> TitleOverviews { get; set; } = new List<TitleOverview>();
-    }
-}
