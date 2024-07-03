c1dec975bf7adda5acb7fd9b6e43e053115ef664
Nobuyuki Akama <nakama@microsoft.com>
2024-07-03 11:07:23 +0900

    ライブラリの最新化、本番環境でサーバ鍵をそろえるための機能を追加

--- AzRefArc.AspNetBlazorServer/Program.cs
+++ AzRefArc.AspNetBlazorServer/Program.cs
@@ -1,5 +1,6 @@
 using AzRefArc.AspNetBlazorServer.Components;
 using AzRefArc.AspNetBlazorServer.Data;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 
@@ -38,6 +39,36 @@ namespace AzRefArc.AspNetBlazorServer
                         providerOptions.EnableRetryOnFailure();
                     });
             });
+
+            // 本番環境用の場合、サーバ間で鍵をそろえる必要がある
+            // 鍵はファイル共有や Keyvault に格納できるが、ここでは追加リソースを構成する手間を避けるために DB に保存してしまうことにする
+            // 本番環境用の DB に対して以下のクエリを実行し、テーブルを追加で作成しておく
+            // CREATE TABLE[dbo].[DataProtectionKeys] ( [ID][int] IDENTITY(1, 1) NOT NULL PRIMARY KEY, [FriendlyName] [varchar] (64) NULL, [Xml][text] NULL)
+            // ※ 開発環境ではこのテーブルが不要になるように、ここでは同一 DB に対して二つの DbContext を使うように実装している
+            // この機能を使う場合には、DataProtection:UseSharedKeyOnDatabase 設定を true にすること
+            //"DataProtection": {
+            //    "UseSharedKeyOnDatabase":  true
+            //}
+            string? useSharedKeyOnDatabase = builder.Configuration["DataProtection:UseSharedKeyOnDatabase"];
+            if (string.IsNullOrEmpty(useSharedKeyOnDatabase) == false && bool.Parse(useSharedKeyOnDatabase))
+            {
+                builder.Services.AddDbContextFactory<DataProtectionKeyDbContext>(opt =>
+                {
+                    // DbContext 構成設定
+                    // https://docs.microsoft.com/ja-jp/ef/core/dbcontext-configuration/#other-dbcontext-configuration
+                    if (builder.Environment.IsDevelopment())
+                    {
+                        opt = opt.EnableSensitiveDataLogging().EnableDetailedErrors();
+                    }
+                    opt.UseSqlServer(
+                        builder.Configuration.GetConnectionString("PubsDbContext"),
+                        providerOptions =>
+                        {
+                            providerOptions.EnableRetryOnFailure();
+                        });
+                });
+                builder.Services.AddDataProtection().PersistKeysToDbContext<DataProtectionKeyDbContext>().SetApplicationName("AzRefArc.AspNetBlazorUnited");
+            }
 
             var app = builder.Build();
 
